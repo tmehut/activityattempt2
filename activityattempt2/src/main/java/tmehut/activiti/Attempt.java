91deeec7894dc5fd35149b00b903f51bb77ba8ae
tmehut <vladeycolder@yahoo.com>
2016-10-20 05:48:16 +0200

    Added activitiy.cfg.xml-based ProcessEngine instatiation

--- activityattempt2/src/main/java/tmehut/activiti/Attempt.java
+++ activityattempt2/src/main/java/tmehut/activiti/Attempt.java
@@ -2,6 +2,7 @@ package tmehut.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
 import org.apache.log4j.BasicConfigurator;
 
 /**
@@ -16,9 +17,21 @@ public class Attempt
     	BasicConfigurator.configure();
         
     	try{
-        	ProcessEngine processEngine = ProcessEngineConfiguration
-        			   .createStandaloneInMemProcessEngineConfiguration()
-        			   .buildProcessEngine();
+        	
+    		/* Creating default standalone ProcessEngine without using activiti.cfg.xml file */
+    		//ProcessEngine processEngine = ProcessEngineConfiguration
+        	//		   .createStandaloneInMemProcessEngineConfiguration()
+        	//		   .buildProcessEngine();
+        	
+    		
+    		/* The easiest way to obtain a ProcessEngine, is to use the org.activiti.engine.ProcessEngines class:
+    		 * ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine()
+			 * This will look for an activiti.cfg.xml file on the classpath and construct an engine based on the configuration 
+			 * in that file. The following snippet shows an example configuration. The following sections will give a detailed 
+			 * overview of the configuration properties.
+			 */    		
+    		 ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
 
         	System.out.println("Process Engine Name = " + processEngine.getName());      	
 		} catch(NoClassDefFoundError e) {
@@ -26,5 +39,5 @@ public class Attempt
 		}
 		
 		System.out.println("Recovered from exception if any");
-    }
+   }
 }
